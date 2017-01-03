f8c813957ca6c6c1148176a1e9bfe9ce14c09288
bkraft <bkraft4257@gmail.com>
2017-01-03 13:25:44 -0500

    Added configuration files for tic_labels.sh

---
+++ other/tic_labels.sh
@@ -0,0 +1,16 @@
+
+export TIC_LABELS_PATH='/Users/bkraft/PycharmProjects/tic_labels'  # Add path information here
+export TIC_LABELS_PYTHONPATH=${TIC_LABELS_PATH}/labels
+
+export PYTHONPATH=${TIC_LABELS_PYTHONPATH}:$PYTHONPATH
+
+alias  tic_labels_cc='python2 ${TIC_LABELS_PYTHONPATH}/connected_components.py'
+alias  tic_labels_create_sphere='python2 ${TIC_LABELS_PYTHONPATH}/create_sphere.py'
+alias  tic_labels_extract='python2 ${TIC_LABELS_PYTHONPATH}/extract.py'
+alias  tic_labels_label='python2 ${TIC_LABELS_PYTHONPATH}/label.py'
+alias  tic_labels_list='python2 ${TIC_LABELS_PYTHONPATH}/list.py'
+alias  tic_labels_remove='python2 ${TIC_LABELS_PYTHONPATH}/remove.py'
+alias  tic_labels_set='python2 ${TIC_LABELS_PYTHONPATH}/set.py'
+alias  tic_labels_sort='python2 ${TIC_LABELS_PYTHONPATH}/sort.py'
+alias  tic_labels_stats='python2 ${TIC_LABELS_PYTHONPATH}/stats.py'
+alias  tic_labels_where='python2 ${TIC_LABELS_PYTHONPATH}/where.py'
